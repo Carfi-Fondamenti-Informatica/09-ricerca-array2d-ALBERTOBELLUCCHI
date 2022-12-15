5b342a069d8a2f297513141d914b35e8890d1964
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-12-15 15:41:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char arr[10][20];
+    char nome[20]={"rty"};
+    int pos_nome=69;
+
+    for(int i=0; i<10; i++){//inizializzo vuoto
+        for(int j=0; j<20; j++){
+            arr[i][j]=' ';
+            nome[j]=' ';
+        }
+    }
+
+    for(int i=0; i<10; i++) cin>> arr[i];//prendo da tastirera
+    cin>>nome;
+
+    if(trova(arr, nome,pos_nome))cout<<pos_nome;
+    else cout<<"non presente";
+
+    return 0;
 }
